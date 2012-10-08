0c1e049b929839f0cfaca694da8d209c785d7cee
lly <lly123@gmail.com>
2012-10-08 17:06:36 +0800

    add multiple teams support

--- overview.rb
+++ overview.rb
@@ -2,38 +2,54 @@ require "rexml/document"
 require "open-uri"
 require "net/http"
 
-MingleServerAndPort = '163.184.134.16:8080'
-MingleUserName = 'admin'
-MinglePassword = '123456'
+module OverviewScript
+  extend self
 
-TemplateName = 'Display_Team_Sprint_Overview'
-MaxWellURI = 'api/v2/projects/maxwell'
-URLForGettingTemplate = "http://#{MingleServerAndPort}/#{MaxWellURI}/wiki/#{TemplateName}.xml"
-URLForAddingWiki = "http://#{MingleUserName}:#{MinglePassword}@#{MingleServerAndPort}/#{MaxWellURI}/wiki.xml"
+  MingleServerAndPort = '163.184.134.16:8080'
+  MingleUserName = 'admin'
+  MinglePassword = '123456'
 
-WikiPageName = 'test wiki page'
+  TemplateNames = ['Display_Team_Sprint_Overview', 'Data1_Team_Sprint_Overview',
+                  'Data2_Team_Sprint_Overview', 'Performance_Team_Sprint_Overview',
+                  'Workflow_DM_Team_Sprint_Overview', 'Workflow_WL_Team_Sprint_Overview',
+                  'Environment_CI_Team_Sprint_Overview']
 
-tagsAndValues = {
-    %r{\(Current Sprint Order\)} => %q{'3'},
-    %r{\(Current Sprint\)} => %q{'Sprint 3'},
-    %r{\(Current Sprint Start Date\)} => %q{'2012-09-20'},
-    %r{\(Current Sprint End Date\)} => %q{'2012-10-01'},
-    %r{\(Current Release\)} => %q{'Release 1'}
-}
+  MaxWellURI = 'api/v2/projects/mingleapitest'
+  URLForAddingWiki = "http://#{MingleUserName}:#{MinglePassword}@#{MingleServerAndPort}/#{MaxWellURI}/wiki.xml"
 
-def replaceTagsWithValues content, tagsAndValues
-  tagsAndValues.to_a.inject(content) { |content, tagAndValue| content.gsub(tagAndValue[0], tagAndValue[1]) }
+  def generateOverviews
+
+    tagsAndValues = {
+        %r{\(Current Sprint Order\)} => %q{'3'},
+        %r{\(Current Sprint\)} => %q{'Sprint 3'},
+        %r{\(Current Sprint Start Date\)} => %q{'2012-09-20'},
+        %r{\(Current Sprint End Date\)} => %q{'2012-10-01'},
+        %r{\(Current Release\)} => %q{'Release 1'}
+    }
+
+    def replaceTagsWithValues content, tagsAndValues
+      tagsAndValues.to_a.inject(content) { |content, tagAndValue| content.gsub(tagAndValue[0], tagAndValue[1]) }
+    end
+
+    def wikiPageName prefix, tagsAndValues
+      prefix + ' | ' + tagsAndValues[%r{\(Current Sprint\)}][1..-2]
+    end
+
+    TemplateNames.each { |templateName|
+      urlForGettingTemplate = "http://#{MingleServerAndPort}/#{MaxWellURI}/wiki/#{templateName}.xml"
+
+      open(urlForGettingTemplate, :http_basic_authentication => [MingleUserName, MinglePassword]) do |f|
+        templatePage = REXML::Document.new(f)
+        templateContent = replaceTagsWithValues(templatePage.elements['page/content'].text, tagsAndValues)
+
+        Net::HTTP.post_form(URI.parse(URLForAddingWiki),
+                            {'page[name]' => wikiPageName(templateName, tagsAndValues),
+                             'page[content]' => templateContent})
+      end
+    }
+  end
+
 end
 
-def wikiPageName prefix, tagsAndValues
-  prefix + ' | ' + tagsAndValues[%r{\(Current Sprint\)}][1..-2]
-end
-
-open(URLForGettingTemplate, :http_basic_authentication => [MingleUserName, MinglePassword]) do |f|
-  templatePage = REXML::Document.new(f)
-  templateContent = replaceTagsWithValues(templatePage.elements['page/content'].text, tagsAndValues)
-
-  Net::HTTP.post_form(URI.parse(URLForAddingWiki),
-                      {'page[name]' => wikiPageName(TemplateName, tagsAndValues), 'page[content]' => templateContent})
-end
+OverviewScript.generateOverviews
 
