2e31bf91a1e6278cd1bf53f5c9cef83c6a9d57ec
Jennifer <twer@twers-MacBook-Pro-2.local>
2012-10-09 14:23:04 +0800

    introduce rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MingleScript::Application.config.secret_token = '76e23f8a454581c2c34d3d9363b97e813fb79f53f88cd7faf852dc004fc64756fad81c5bccc26fd1eff7adff9b3fad8c2f8919bd515553a93076c42f8a044305'
