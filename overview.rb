565873788d7c7802f2176db882a3aba9c4e814e6
lly <lly123@gmail.com>
2012-10-09 15:19:22 +0800

    add update team overview list

--- overview.rb
+++ overview.rb
@@ -5,7 +5,9 @@ require "net/http"
 module OverviewScript
   extend self
 
-  MingleServerAndPort = '163.184.134.16:8080'
+  MingleServer = '163.184.134.16'
+  MingleServerPort = '8080'
+  MingleServerAndPort = "#{MingleServer}:#{MingleServerPort}"
   MingleUserName = 'admin'
   MinglePassword = '123456'
 
@@ -14,11 +16,12 @@ module OverviewScript
                   'Workflow_DM_Team_Sprint_Overview', 'Workflow_WL_Team_Sprint_Overview',
                   'Environment_CI_Team_Sprint_Overview']
 
-  MaxWellURI = 'api/v2/projects/mingleapitest'
-  ScriptTemplatesURI = "api/v2/projects/mingle_script_templates"
-  URLForAddingWiki = "http://#{MingleUserName}:#{MinglePassword}@#{MingleServerAndPort}/#{MaxWellURI}/wiki.xml"
+  MaxWellURI = '/api/v2/projects/mingleapitest'
 
   def generateOverviews releaseOrder, sprintOrder, sprintStartDate, sprintEndDate
+
+    scriptTemplatesURI = "/api/v2/projects/mingle_script_templates"
+    urlForAddingWiki = "http://#{MingleUserName}:#{MinglePassword}@#{MingleServerAndPort}#{MaxWellURI}/wiki.xml"
 
     tagsAndValues = {
         %r{\(Current Sprint Order\)} => %Q{"#{sprintOrder}"},
@@ -33,24 +36,79 @@ module OverviewScript
     end
 
     def wikiPageName prefix, tagsAndValues
-      prefix + ' | ' + tagsAndValues[%r{\(Current Sprint\)}][1..-2]
+      prefix + ' - ' + tagsAndValues[%r{\(Current Sprint\)}][1..-2]
     end
 
     TemplateNames.each { |templateName|
-      urlForGettingTemplate = "http://#{MingleServerAndPort}/#{ScriptTemplatesURI}/wiki/#{templateName}.xml"
+      urlForGettingTemplate = "http://#{MingleServerAndPort}#{scriptTemplatesURI}/wiki/#{templateName}.xml"
 
       open(urlForGettingTemplate, :http_basic_authentication => [MingleUserName, MinglePassword]) do |f|
         templatePage = REXML::Document.new(f)
         templateContent = replaceTagsWithValues(templatePage.elements['page/content'].text, tagsAndValues)
 
-        Net::HTTP.post_form(URI.parse(URLForAddingWiki),
+        Net::HTTP.post_form(URI.parse(urlForAddingWiki),
                             {'page[name]' => wikiPageName(templateName, tagsAndValues),
                              'page[content]' => templateContent})
       end
     }
   end
 
+  def requestAPI apiURI, xpath
+    Enumerator.new do |y|
+      open("http://#{MingleServerAndPort}#{apiURI}", :http_basic_authentication => [MingleUserName, MinglePassword]) do |f|
+        REXML::Document.new(f).each_element(xpath) do |e|
+          y << e.text
+        end
+      end
+    end
+  end
+
+  def findHowManySprintOverviewsAlreadyInTheProject
+    requestAPI("#{MaxWellURI}/wiki.xml", %q{//page[contains(name, ' - Sprint')]/name}).inject([]) { |numbers, text|
+      numbers << text[/\s-\sSprint\s([0-9]+)$/, 1]
+    }.uniq
+  end
+
+  def generateContentOfSprintOverviewList teamName, sprintNumbers
+    header = <<-HEADER
+        {% dashboard-panel %}
+        {% panel-heading %}#{teamName} Sprint Overview{% panel-heading %}
+        {% panel-content %}
+    HEADER
+
+    footer = <<-FOOTER
+        {% panel-content %}
+        {% dashboard-panel %}
+    FOOTER
+
+    sprintNumbers.inject(header) { |content, number|
+      content << "[[#{teamName} Sprint Overview - Sprint #{number}]]<br/>\n"
+    } << footer
+  end
+
+  def getTeamName templateName
+    templateName[/(\w+)_Sprint_Overview/, 1].gsub(/_/, ' ')
+  end
+
+  def updateOverview templateName, content
+    uriOfOverviewList = "#{MaxWellURI}/wiki/#{templateName}.xml"
+
+    putRequest = Net::HTTP::Put.new(uriOfOverviewList)
+    putRequest.basic_auth MingleUserName, MinglePassword
+    putRequest.body = "page[content]=#{content}"
+
+    Net::HTTP.new(MingleServer, MingleServerPort).start { |http| http.request(putRequest) }
+  end
+
+  def updateOverviewList
+    TemplateNames.each { |templateName|
+      sprintNumbers = findHowManySprintOverviewsAlreadyInTheProject
+      overviewContent = generateContentOfSprintOverviewList(getTeamName(templateName), sprintNumbers)
+      updateOverview(templateName, overviewContent)
+    }
+  end
+
 end
 
-OverviewScript.generateOverviews ARGV[0], ARGV[1], ARGV[2], ARGV[3]
+#OverviewScript.generateOverviews ARGV[0], ARGV[1], ARGV[2], ARGV[3]
 
